3b0321754827a090aab2bd0d91e56bdd3180d550
JamesS <kolliejames118@gmail.com>
2024-12-21 02:35:21 +0000

    added netlify.tmol

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './',
   server: {
-    port: 8000,
-    // Add this if you want Vite to automatically try the next available port if 8000 is in use
+    port: 8000,  // Add this if you want Vite to automatically try the next available port if 8000 is in use
     strictPort: false
   }
 })
